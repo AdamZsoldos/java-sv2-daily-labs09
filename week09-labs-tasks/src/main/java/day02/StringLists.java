9ae3fcd237f4eaa7cb8f54701ba311cbc6ab7e10
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-12-07 15:41:49 +0100

    Added day02.StringLists and StringListsTest

---
+++ week09-labs-tasks/src/main/java/day02/StringLists.java
@@ -0,0 +1,22 @@
+package day02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringLists {
+
+    public List<String> getShortestWords(List<String> words) {
+        List<String> result = new ArrayList<>();
+        int minLength = Integer.MAX_VALUE;
+        for (String word : words) {
+            if (word.length() < minLength) {
+                minLength = word.length();
+                result = new ArrayList<>();
+            }
+            if (word.length() == minLength) {
+                result.add(word);
+            }
+        }
+        return result;
+    }
+}
